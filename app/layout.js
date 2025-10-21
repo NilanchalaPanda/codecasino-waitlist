3657b5595ef3247a8d3b3f298e3c33b5362dd1b9
NilanchalaPanda <nilanchalpanda2003@gmail.com>
2025-10-21 21:56:31 +0530

    feat: inital commit

---
+++ app/layout.js
@@ -0,0 +1,33 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "CodeCasino | Get In Now!",
+  description:
+    "Where Code Meets Competition. Join the waitlist and enter the ultimate coding arena.",
+  icons: {
+    icon: "/favicon.ico", // favicon will automatically be picked up
+  }
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
